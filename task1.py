a5639e1e90a33799c0fb3946cb11632bbc34685c
sohamm2410 <sohambajad2410@gmail.com>
2026-01-09 19:01:40 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+num1=int(input('enter a first number:'))
+num2=int(input('enter a second number:'))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print('the addition is',add)
+print('the substraction is',sub)
+print('the Multiplication is',mul)
+print('the Division  is',div)
---
+++ task2.py
@@ -0,0 +1,6 @@
+First_name=input('Enter your First name:')
+Last_name=input('Enter your Last name:')
+
+full_name=First_name+' '+Last_name
+print("hello ," , full_name, "!welcome to python programming")
+
